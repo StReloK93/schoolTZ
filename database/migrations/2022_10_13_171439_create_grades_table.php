9092805b5b16e6f7f8a68d727a924d7d4410d697
strelok93 <strelok0493@gmail.com>
2022-10-13 23:50:30 +0500

    preparation

--- database/migrations/2022_10_13_171439_create_grades_table.php
+++ database/migrations/2022_10_13_171439_create_grades_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('grades', function (Blueprint $table) {
             $table->id();
+            $table->integer('student_id');
+            $table->integer('subject_id');
+            $table->integer('grade');
             $table->timestamps();
         });
     }
