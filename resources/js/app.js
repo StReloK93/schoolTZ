7ab4f4a58570247723a9cd452b782e16c155d3c3
strelok93 <strelok0493@gmail.com>
2022-10-13 22:29:56 +0500

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import {createApp} from 'vue'
+
+import App from './main/App.vue'
+import './main/app.css'
+
+
+createApp(App).mount("#app")
---
+++ resources/js/main/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-red-600">
+        Hello moto
+    </div>
+</template>
+
+<script setup>
+
+</script>
